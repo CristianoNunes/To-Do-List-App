26b9f2d33018b81092dd0f507efb8be318673147
CristianoNunes <cristianonunes99@gmail.com>
2024-07-07 11:13:57 -0300

    Fixed test suite configuration

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy",
