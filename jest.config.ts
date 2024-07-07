09a2e3dc687ccdb085908edbb10386a8fbe3b82b
CristianoNunes <cristianonunes99@gmail.com>
2024-07-07 10:39:15 -0300

    Installed and configured React Testing Library

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy",
   },
