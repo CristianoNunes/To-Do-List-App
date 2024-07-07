bf6434964bfd0d69441b0437983eb805e8347ac8
CristianoNunes <cristianonunes99@gmail.com>
2024-07-07 08:42:44 -0300

    Implemented TaskTemplate

---
+++ src/templates/TaskTemplate.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Input from "../components/atoms/Input";
+import Button from "../components/atoms/Button";
+import TaskList from "../components/organisms/TaskList";
+import { Task } from "../types/Task";
+
+interface TaskTemplateProps {
+  tasks: Task[];
+  newTask: string;
+  onAddTask: () => void;
+  onNewTaskChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onToggleTask: (id: string) => void;
+}
+
+const TaskTemplate: React.FC<TaskTemplateProps> = ({
+  tasks,
+  newTask,
+  onAddTask,
+  onNewTaskChange,
+  onToggleTask,
+}) => (
+  <div className="p-4">
+    <div className="flex space-x-2 mb-4">
+      <Input value={newTask} onChange={onNewTaskChange} />
+      <Button onClick={onAddTask}>Add Task</Button>
+    </div>
+    <TaskList tasks={tasks} onToggleTask={onToggleTask} />
+  </div>
+);
+
+export default TaskTemplate;
